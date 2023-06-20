a7596c4af3dcdf21be11030d00110a63d275e3da
Caleb Kirschbaum <calebhk98@gmail.com>
2023-06-19 21:52:59 -0500

    Automattically downloads python if not installed

--- RunThis.bat
+++ RunThis.bat
@@ -1,5 +1,26 @@
 @echo off
 
+python --version >nul 2>&1
+
+if errorlevel 1 (
+    echo Python is not installed or not in the system PATH.
+    echo Attempting to download and install Python...
+
+    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe' -OutFile 'python_installer.exe'"
+    python_installer.exe /passive /norestart InstallAllUsers=1 PrependPath=1
+
+    if errorlevel 1 (
+        echo Failed to install Python. Please visit https://www.python.org/downloads/ and install it manually.
+        exit /b
+    )
+
+    echo Successfully installed Python.
+    del python_installer.exe
+
+    echo Adding Python to the PATH.
+    set "PATH=%PATH%;%localappdata%\Programs\Python\Python310\Scripts;%localappdata%\Programs\Python\Python310\"
+)
+
 if not exist "venv" (
     echo Creating virtual environment...
     python -m venv venv
